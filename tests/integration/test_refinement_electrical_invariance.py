92eee3261965f427da8d7ba84647aa0284d3c464
Phillip Chin <ekkus93@gmail.com>
2026-08-11 23:49:57 -0700

    test: preserve authored divider net names in KiCad

--- tests/integration/test_refinement_electrical_invariance.py
+++ tests/integration/test_refinement_electrical_invariance.py
@@ -49,6 +49,7 @@ def test_real_kicad_export_preserves_generated_divider_electrically(tmp_path: Pa
             symbols_dir=str(symbols_dir),
             mode="internal",
             layout="graphviz",
+            label_mode="debug",
         )
     )
 
